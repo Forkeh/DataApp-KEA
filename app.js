dc75baa77da0379146465e7fe38c23465f81049a
Forkeh <forkeh@gmail.com>
2023-03-23 13:57:00 +0100

    Made jimmy object into json file, can read from it and add to grid

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 
 window.addEventListener("load", initApp);
 
-function initApp() {
-  showCharacter(jimmyValmer);
-  showCharacter(jimmyValmer);
-  showCharacter(jimmyValmer);
-  showCharacter(jimmyValmer);
-  showCharacter(jimmyValmer);
-  showCharacter(jimmyValmer);
+async function initApp() {
+  const jimmy = await getCharacterData('/data/jimmy.json');
+  console.log(jimmy);
+  showCharacter(jimmy);
+  showCharacter(jimmy);
+  showCharacter(jimmy);
+  showCharacter(jimmy);
+  showCharacter(jimmy);
+  showCharacter(jimmy);
 }
 
 //TODO:
-function getData() {
+async function getCharacterData(url) {
   console.log("GET DATA");
+const response = await fetch(url);
+const data = await response.json();
+return data
 }
 
 function showCharacter(character) {
@@ -67,22 +72,3 @@ function showModalCharacter(character) {
     character.appearances;
   document.querySelector(".dialog-episodes").textContent = character.episodes;
 }
-
-const jimmyValmer = {
-  name: "Jimmy Valmer",
-  nickname: "The Bard, Fastpass",
-  image:
-    "http://southparkstudios.mtvnimages.com/shared/characters/kids/jimmy-valmer.png?height=165",
-  occupation: "Student, Comedian, Journalist, Ad Blocker",
-  age: 10,
-  voicedBy: "Trey Parker",
-  gender: "Male",
-  religion: "Protestant",
-  catchPhrase: "Wow, what a great audience.",
-  hairColor: "Brown",
-  schoolGrade: 4,
-  episodes:
-    "S05E02, S06E10, S07E05, S07E11, S08E10, S09E14, S10E07, S11E06, S11E14, S12E03, S13E02, S13E03, S13E14, S14E07, S15E07, S15E08, S16E11, S17E04, S17E05, S18E07, S19E06, S20E02, S20E03, S20E04, S20E05, S20E06, S20E07, S20E08, S20E09, S20E10",
-  appearances: 30,
-  firstAppearance: "S05E02",
-};
